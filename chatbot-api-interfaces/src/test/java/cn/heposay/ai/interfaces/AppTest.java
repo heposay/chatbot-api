fa09272587cddcbba747db2905677293824220ff
Mrlin <291960665@qq.com>
2023-04-26 13:53:29 +0800

    小波 fix: 修复bug

--- chatbot-api-interfaces/src/test/java/cn/heposay/ai/interfaces/AppTest.java
+++ chatbot-api-interfaces/src/test/java/cn/heposay/ai/interfaces/AppTest.java
@@ -1,5 +1,6 @@
 package cn.heposay.ai.interfaces;
 
+import okhttp3.*;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -10,6 +11,8 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -21,7 +24,9 @@ import java.io.IOException;
  */
 public class AppTest {
 
-    private static final String OPEN_AI_KEY = "xxxx";
+    Logger logger = LoggerFactory.getLogger(AppTest.class);
+
+    private static final String OPEN_AI_KEY = "xxx";
     private static final String COOKIE = "UM_distinctid=186a1b7b06611c-03507ddfe121e9-1d525635-1aeaa0-186a1b7b067740; zsxq_access_token=037C20AA-B1A1-CA8D-3BCD-230A2A9B50A9_25AFFA98B3660AC6; sensorsdata2015jssdkcross={\\\"distinct_id\\\":\\\"15158212415442\\\",\\\"first_id\\\":\\\"18691e9387a3cc-052d94d2fb0371-1d525635-1764000-18691e9387bdf6\\\",\\\"props\\\":{\\\"$latest_traffic_source_type\\\":\\\"引荐流量\\\",\\\"$latest_search_keyword\\\":\\\"未取到值\\\",\\\"$latest_referrer\\\":\\\"https://bugstack.cn/\\\"},\\\"identities\\\":\\\"eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTg2OTFlOTM4N2EzY2MtMDUyZDk0ZDJmYjAzNzEtMWQ1MjU2MzUtMTc2NDAwMC0xODY5MWU5Mzg3YmRmNiIsIiRpZGVudGl0eV9sb2dpbl9pZCI6IjE1MTU4MjEyNDE1NDQyIn0=\\\",\\\"history_login_id\\\":{\\\"name\\\":\\\"$identity_login_id\\\",\\\"value\\\":\\\"15158212415442\\\"},\\\"$device_id\\\":\\\"18691e9387a3cc-052d94d2fb0371-1d525635-1764000-18691e9387bdf6\\\"}; zsxqsessionid=8e56eb544ab2592f20760dcd5b29ecca; abtest_env=product";
 
 
@@ -35,11 +40,10 @@ public class AppTest {
         CloseableHttpResponse response = httpClient.execute(get);
         if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
             String res = EntityUtils.toString(response.getEntity());
-            System.out.println(res);
+            logger.info("请求成功，结果：{}" + res);
         } else {
-            System.out.println(response.getStatusLine().getStatusCode());
+            logger.error("请求失败，失败原因：{}", response.getStatusLine().getStatusCode());
         }
-
     }
 
     @Test
@@ -62,39 +66,36 @@ public class AppTest {
         CloseableHttpResponse response = httpClient.execute(post);
         if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
             String res = EntityUtils.toString(response.getEntity());
-            System.out.println(res);
+            logger.info("请求成功，结果：{}" + res);
         } else {
-            System.out.println(response.getStatusLine().getStatusCode());
+            logger.error("请求失败，失败原因：{}", response.getStatusLine().getStatusCode());
         }
+
     }
 
     @Test
     public void test_openapi() throws IOException {
         CloseableHttpClient httpClient = HttpClientBuilder.create().build();
 
-        HttpPost post = new HttpPost("https://api.openai.com/v1/completions");
+        HttpPost post = new HttpPost("https://api.openai.com/v1/chat/completions");
         post.addHeader("Authorization", "Bearer "+ OPEN_AI_KEY);
-        post.addHeader("Content-Type", "application/json;charset=UTF-8");
+        post.addHeader("Content-Type", "application/json");
 
-        String paramJson = "{\n" +
-                "        \"model\": \"text-davinci-003\",\n" +
-                "                \"prompt\": \"帮我写一个冒泡排序算法\",\n" +
-                "                \"max_tokens\": 1024,\n" +
-                "                \"temperature\": 0\n" +
-                "    }";
+        String paramJson = "{\"model\": \"gpt-3.5-turbo\",\"messages\": [{\"role\": \"user\",\"content\": \"帮我写一个冒泡排序!\"}]}";
 
         StringEntity stringEntity = new StringEntity(paramJson,
-                ContentType.create("text/json", "UTF-8"));
+                ContentType.create("application/json", "UTF-8"));
 
         post.setEntity(stringEntity);
 
         CloseableHttpResponse response = httpClient.execute(post);
         if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
             String res = EntityUtils.toString(response.getEntity());
-            System.out.println(res);
+            logger.info("请求成功，结果：{}" + res);
         } else {
-            System.out.println(response.getStatusLine().getStatusCode());
+            logger.error("请求失败，失败原因：{}", response.getStatusLine().getStatusCode());
         }
+
 
     }
 
